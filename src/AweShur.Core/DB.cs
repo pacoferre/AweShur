f017dae9cad67269a3f856e12caa7f43f45cf177
Paco Ferre <pacoferre@gmail.com>
2016-06-16 23:15:55 +0200

    Transaction support.

--- src/AweShur.Core/DB.cs
+++ src/AweShur.Core/DB.cs
@@ -10,29 +10,44 @@ using Microsoft.Extensions.Configuration;
 
 namespace AweShur.Core
 {
-    public class DB
+    public class DB : IDisposable
     {
         public static IConfigurationRoot Configuration { get; set; }
 
         public DBDialect Dialect { get; }
         public int DBNumber { get; }
         private string connectionString;
+        private Lazy<IDbConnection> lazyConnection;
+        private IDbConnection conn = null;
+        private IDbTransaction trans = null;
+        private bool inTransaction = false;
+        private int secondsTimeOut = 240;
 
         public DB(int dbNumber)
         {
-            //      "Data": {
-            //          "Instance_0": {
-            //              "Dialect": "SQLServer",
-            //              "ConnectionString": "..."
-            //          }
-            //      }
-
-            IConfigurationSection section = Configuration.GetSection("AppSettings").GetSection("Instance_" + dbNumber);
+            IConfigurationSection section = Configuration.GetSection("Data").GetSection("Instance_" + dbNumber);
 
             DBNumber = dbNumber;
 
             Dialect = DBDialect.Instance((DBDialectEnum) Enum.Parse(typeof(DBDialectEnum), section["Dialect"]));
             connectionString = section["ConnectionString"];
+
+            lazyConnection = new Lazy<IDbConnection>(() => {
+                return Dialect.GetConnection(connectionString);
+            }, false);
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                CloseConnection(true);
+                conn.Dispose();
+                conn = null;
+            }
+            catch
+            {
+            }
         }
 
         public static DB Instance
@@ -58,29 +73,124 @@ namespace AweShur.Core
             return (DB)context.Items[key];
         }
 
-        private IDbConnection Open()
+        private void OpenConnection()
         {
-            return Dialect.GetConnection(connectionString);
+            if (conn == null)
+            {
+                conn = lazyConnection.Value;
+            }
+
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            if (!inTransaction)
+            {
+                CloseConnection(false);
+            }
+        }
+
+        private void CloseConnection(bool forceClose)
+        {
+            lock (this)
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    if (inTransaction && forceClose)
+                    {
+                        RollBackTransaction();
+
+                        throw new Exception("Can't close with open transaction. RollBack done.");
+                    }
+                    else
+                    {
+                        if (!inTransaction)
+                        {
+                            conn.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void BeginTransaction()
+        {
+            lock (this)
+            {
+                if (inTransaction)
+                {
+                    throw new System.Exception("Can't start transaction twice.");
+                }
+
+                OpenConnection();
+                trans = conn.BeginTransaction();
+                inTransaction = true;
+            }
+        }
+
+        public bool InTransaction
+        {
+            get
+            {
+                return inTransaction;
+            }
+        }
+
+        public void RollBackTransaction()
+        {
+            lock (this)
+            {
+                if (!inTransaction)
+                {
+                    throw new System.Exception("Can't Rollback without BeginTransaction.");
+                }
+
+                trans.Rollback();
+                inTransaction = false;
+                trans = null;
+                CloseConnection();
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            if (!inTransaction)
+            {
+                throw new System.Exception("Can't Commit without BeginTransaction.");
+            }
+
+            trans.Commit();
+            inTransaction = false;
+            trans = null;
+            CloseConnection();
         }
 
         public IEnumerable<T> Query<T>(string sql, object param = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
         {
-            using (IDbConnection conn = Open())
-            {
-                return conn.Query<T>(sql, param, null, buffered, commandTimeout, commandType);
-            }
+            IEnumerable<T> result;
+
+            OpenConnection();
+
+            result = conn.Query<T>(sql, param, trans, buffered, commandTimeout, commandType);
+
+            CloseConnection();
+
+            return result;
         }
 
         public void ReadBusinessObject(BusinessBase obj)
         {
-            using (IDbConnection conn = Open())
-            {
-                BusinessBaseDefinition def = obj.Definition;
+            BusinessBaseDefinition def = obj.Definition;
 
-                conn.ReadBusinessObject(obj, def.SelectQuery, def.GetPrimaryKeyParameters(obj));
+            OpenConnection();
 
-                conn.Close();
-            }
+            conn.ReadBusinessObject(obj, def.SelectQuery, def.GetPrimaryKeyParameters(obj), trans);
+
+            CloseConnection();
         }
     }
 }
