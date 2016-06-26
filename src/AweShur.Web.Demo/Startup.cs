938908cca7d7e9062898f17f15ad0468cdbe326f
Paco Ferre <pacoferre@gmail.com>
2016-06-26 09:06:58 +0200

    Missing files! Why if .gitignore is ok?

---
+++ src/AweShur.Web.Demo/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using AweShur.Web.Demo.Models;
+using AweShur.Core;
+using AweShur.Web.Demo.Controllers;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching;
+using StackExchange.Redis;
+
+namespace AweShur.Web.Demo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSession(o => {
+                IConfigurationSection section = Configuration.GetSection("Session");
+                int minutes = section["Expiration"].NoNullInt();
+                string cookieName = section["CookieName"] ?? ".AWSD";
+
+                if (minutes == 0)
+                {
+                    minutes = 10;
+                }
+
+                o.IdleTimeout = TimeSpan.FromMinutes(minutes);
+                o.CookieName = cookieName;
+
+                AweShur.Web.Helpers.CookiesHelper.CookieName = cookieName;
+            });
+
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseMvc(routes =>
+            {
+                AweShur.Web.Startup.AddRoutes(routes);
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+            BusinessBaseProvider.Configure(
+                app.ApplicationServices.GetRequiredService<IHttpContextAccessor>(),
+                redis, new AweShurDemoProvider(), Configuration);
+        }
+    }
+}
---
+++ src/AweShur.Web/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace AweShur.Web
+{
+    public class Startup
+    {
+        public static void AddRoutes(IRouteBuilder routes)
+        {
+            routes.MapRoute(
+                name: "crudList",
+                template: "CRUD/List/{objectName}",
+                defaults: new { controller = "CRUD", action = "List" });
+            routes.MapRoute(
+                name: "crudPost",
+                template: "CRUD/Post",
+                defaults: new { controller = "CRUD", action = "Post" });
+            routes.MapRoute(
+                name: "elements1",
+                template: "Elements/Load/{folder}/{component}/{id}",
+                defaults: new { controller = "Elements", action = "LoadFolder" });
+            routes.MapRoute(
+                name: "elements",
+                template: "Elements/Load/{component}/{id}",
+                defaults: new { controller = "Elements", action = "Load" });
+            routes.MapRoute(
+                name: "AWSLib",
+                template: "AWSLib/{component}",
+                defaults: new { controller = "Elements", action = "AWSLib" });
+        }
+    }
+}
