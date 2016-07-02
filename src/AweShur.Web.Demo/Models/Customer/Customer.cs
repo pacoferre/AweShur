251989463373780a346277c854f5786304859a8e
Paco Ferre <pacoferre@gmail.com>
2016-07-02 12:54:20 +0200

    Missing file (Visual Studio and git problem?) and SQL Server script.

---
+++ src/AweShur.Web.Demo/Models/Customer/Customer.cs
@@ -0,0 +1,41 @@
+﻿using AweShur.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AweShur.Web.Demo.Models.Customer
+{
+    public class CustomerDecorator : BusinessBaseDefinition
+    {
+        protected override void SetCustomProperties()
+        {
+            PropertyDefinition prop = Properties["idCustomerType"];
+
+            prop.Type = PropertyInputType.select; // Will not be usefull untill we got HtmlHelpers
+            prop.DefaultValue = BusinessBaseProvider.ListProvider.GetList("CustomerType").First[0];
+        }
+    }
+
+    public class Customer : BusinessBase
+    {
+        public Customer() : base("Customer")
+        {
+        }
+
+        public override string Description
+        {
+            get
+            {
+                int idCustomerType = this["idCustomerType"].NoNullInt();
+
+                if (idCustomerType > 0)
+                {
+                    return this["name"] + " (" + BusinessBaseProvider.ListProvider.GetList("CustomerType").GetValue(idCustomerType) + ")";
+                }
+
+                return this["name"].ToString();
+            }
+        }
+    }
+}
