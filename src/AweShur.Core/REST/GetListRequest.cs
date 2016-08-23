4ca8138edaa7c2515c7a15e5894443e51e379ef8
Paco Ferre <pacoferre@gmail.com>
2016-08-23 19:33:02 +0200

    New list provider system.

---
+++ src/AweShur.Core/REST/GetListRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AweShur.Core.REST
+{
+    public class GetListRequest
+    {
+        public string objectName { get; set; }
+        public string listName { get; set; }
+        public string parameter { get; set; }
+        public string other { get; set; }
+    }
+}
